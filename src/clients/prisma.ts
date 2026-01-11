ff93af1bb00012cea0daf5ce53ee39b748b11b34
Vadym Bondarenko <v.bondarenko.job@gmail.com>
2026-01-11 20:27:31 +0200

    feat(orm): configure prisma

--- src/clients/prisma.ts
+++ src/clients/prisma.ts
@@ -1,13 +1,18 @@
-// import { PrismaClient } from '.prisma/client';
+import { PrismaClient } from '@generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 class Prisma {
-  // private static instance: PrismaClient;
-  // public static get connection(): PrismaClient {
-  //   if (!Prisma.instance) {
-  //     Prisma.instance = new PrismaClient();
-  //   }
-  //   return Prisma.instance;
-  // }
+  private static instance: PrismaClient;
+
+  public static get connection(): PrismaClient {
+    if (!Prisma.instance) {
+      const adapter = new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
+      });
+      Prisma.instance = new PrismaClient({ adapter });
+    }
+    return Prisma.instance;
+  }
 }
 
 export default Prisma;
